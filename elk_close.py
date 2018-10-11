f9edebe8862080de62ea00c6f095a48be8acf4e1
牵魂断泪 <454175799@qq.com>
2018-10-11 10:34:57 +0800

    Update elk_close.py

--- elk_close.py
+++ elk_close.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python
-
-import os,re,datetime
-
-#检索ES所有打开的索引
-#提取出来的格式为（项目名-2018.10.10）
-elk_index = os.popen("curl -XGET http://172.16.41.115:9200/_cat/indices?v |grep open |awk '{print $3}'").read().split()
+#author:hong
+#update:2018.10.10
+#description: elk日志处理，7天以上的关闭索引，30天以上的删除索引
 
 
-for index in elk_index:
+import os,re,datetime,requests,time
+#elasticsearch ip地址
+es_ip = "172.16.41.115"
+#关闭索引天数
+close_day = 7
+#删除索引天数
+delete_day = 30
+#获取当前时间
+date_now = datetime.datetime.now()
+
+def elk_close(es_ip,close_day):
+    elk_open_index = os.popen("curl -XGET http://%s:9200/_cat/indices?v |grep open |awk '{print $3}'" %(es_ip)).read().split()
+    for index in elk_open_index:
         #正则匹配出日期(2018.10.10)
         index_date_str = re.findall(r'(\d*\.\d*\.\d*)',index)
         if index_date_str:
-                #转换日期格式
-                index_date = datetime.datetime.strptime(index_date_str[0],'%Y.%m.%d')
-                date_now = datetime.datetime.now()
-                #距离当前为几天
-                delta = date_now - index_date
-                if delta.days > 6:
-                        #关闭7天前打开的索引
-                        index_close = 'http://172.16.41.115:9200/%s/_close' %(index)
-                        os.popen("curl -XPOST %s" %(index_close))
+            index_date = datetime.datetime.strptime(index_date_str[0], '%Y.%m.%d')
+            #与现在相差天数
+            delta = (date_now - index_date).days
+            if delta > close_day:
+                index_close = 'http://%s:9200/%s/_close' % (es_ip,index)
+                os.popen("curl -XPOST %s" % (index_close))
 
- 
+def elk_delete(es_ip,delete_day):
+    elk_all_index = os.popen("curl -XGET http://%s:9200/_cat/indices?v |awk '{print $3}'" %(es_ip)).read().split()
+    for index in elk_all_index:
+        #正则匹配出日期(2018.10.10)
+        index_date_str = re.findall(r'(\d*\.\d*\.\d*)',index)
+        if index_date_str:
+            index_date = datetime.datetime.strptime(index_date_str[0], '%Y.%m.%d')
+            #与现在相差天数
+            delta = (date_now - index_date).days
+            if delta > delete_day:
+                index_delete = 'http://%s:9200/%s/_close' % (es_ip,index)
+                os.popen("curl -XPOST %s" % (index_de))
+
+
+elk_close(es_ip,close_day)
+elk_delete(es_ip,delete_day)
